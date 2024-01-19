81bb4174ca4507cd3716df94dee5cc164fc8123c
Marcus Vinícius <marcus-vnt@live.com>
2024-01-18 23:15:50 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <div>
+        <h1>Vida me Surpreenda!</h1>
+      </div>
+    </main>
+  );
+}
+
+export default App;
