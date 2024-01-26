8b6c72093dec3fdf29ae2b01470dda1c30ef23aa
Marcus Vinícius <marcus-vnt@live.com>
2024-01-26 17:39:16 -0300

    Validate Token, auto login and navigate.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
+import LoginForm from "./Components/Login/LoginForm";
 import Footer from "./Components/footer";
 import { UserStorage } from "./UserContext";
 
@@ -16,6 +17,8 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login/*" element={<Login />} />
+            <Route path="conta" element={<h1>Minha Conta</h1>} />
+            <Route path="*" element={<h1>Not Found 404</h1>} />
           </Routes>
           <Footer />
         </UserStorage>
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import LoginForm from "./LoginForm";
 import FormSignUp from "./FormSignUp";
 import FormPasswordLost from "./FormPasswordLost";
 import FormPasswordReset from "./FormPasswordReset";
+import { UserContext } from "../../UserContext";
 
 const Login = () => {
+  const { login } = React.useContext(UserContext);
+  if (login === true) return <Navigate to="/conta" />;
   return (
     <div>
       <Routes>
-        <Route path="/" element={<LoginForm />} />
+        <Route path="" element={<LoginForm />} />
         <Route path="sign-up" element={<FormSignUp />} />
         <Route path="password-lost" element={<FormPasswordLost />} />
         <Route path="password-reset" element={<FormPasswordReset />} />
