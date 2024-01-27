e84ec3e010c460a7ec9c0f8d845b228d22650ec6
Marcus Vinícius <marcus-vnt@live.com>
2024-01-27 19:29:07 -0300

    ProtectedRouter added

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
-
 import Footer from "./Components/footer";
 import { UserStorage } from "./UserContext";
+import ProtectedRouter from "./Components/Helper/ProtectedRouter";
+import User from "./Components/User/User";
 
 function App() {
   return (
@@ -16,8 +17,15 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
-            <Route path="conta" element={<h1>Minha Conta</h1>} />
+            <Route path="login/*" element={<Login />} />
+            <Route
+              path="conta/*"
+              element={
+                <ProtectedRouter>
+                  <User />
+                </ProtectedRouter>
+              }
+            />
             <Route path="*" element={<h1>Not Found 404</h1>} />
           </Routes>
           <Footer />
