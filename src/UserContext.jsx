e2408b4e5a9839450b981a0d0fc220dbc82cb23f
Marcus Vinícius <marcus-vnt@live.com>
2024-01-27 21:07:34 -0300

    User component / UserHeader + UserHeaderNav

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -10,19 +10,13 @@ export const UserStorage = ({ children }) => {
   const [error, setError] = React.useState(null);
   const navigate = useNavigate();
 
-  const userLogout = React.useCallback(
-    async function () {
-      setData(null);
-      setError(null);
-      setLoading(false);
-      setLogin(false);
-      window.localStorage.removeItem("token");
-      setTimeout(() => {
-        navigate("/login");
-      }, 0);
-    },
-    [navigate]
-  );
+  const userLogout = React.useCallback(async function () {
+    setData(null);
+    setError(null);
+    setLoading(false);
+    setLogin(false);
+    window.localStorage.removeItem("token");
+  }, []);
 
   async function getUser(token) {
     const { url, options } = USER_GET(token);
