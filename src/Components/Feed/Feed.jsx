067d27e9b00701f483ae6f1265937e3ccf63a7ac
Marcus Vinícius <marcus-vnt@live.com>
2024-01-31 18:28:09 -0300

    Creating FeedModal.

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -4,10 +4,13 @@ import FeedModal from "./FeedModal";
 import FeedPhotos from "./FeedPhotos";
 
 const Feed = () => {
+  const [modalPhoto, setModalPhoto] = React.useState(null);
   return (
     <div className={styles.feed}>
-      <FeedModal />
-      <FeedPhotos />
+      {modalPhoto && (
+        <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />
+      )}
+      <FeedPhotos setModalPhoto={setModalPhoto} />
     </div>
   );
 };
