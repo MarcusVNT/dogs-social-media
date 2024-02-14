fd84b17ed1260d73a6eb909df67b4dc29ac2e2ab
Marcus Vinícius <marcus-vnt@live.com>
2024-02-13 22:19:20 -0300

    Feed PropTypes.

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./Feed.module.css";
 import FeedModal from "./FeedModal";
 import FeedPhotos from "./FeedPhotos";
+import PropTypes from "prop-types";
 
 const Feed = ({ user }) => {
   const [modalPhoto, setModalPhoto] = React.useState(null);
@@ -60,6 +61,17 @@ const Feed = ({ user }) => {
       )}
     </div>
   );
+};
+
+Feed.defaultProps = {
+  user: 0,
+};
+
+Feed.propTypes = {
+  user: PropTypes.oneOfType([
+    PropTypes.string.isRequired,
+    PropTypes.number.isRequired,
+  ]),
 };
 
 export default Feed;
