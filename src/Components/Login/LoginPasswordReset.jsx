ec66e9c043186312703abab4aa22075b1505792d
Marcus Vinícius <marcus-vnt@live.com>
2024-02-14 23:22:58 -0300

    Password Reset page.

--- src/Components/Login/LoginPasswordReset.jsx
+++ src/Components/Login/LoginPasswordReset.jsx
@@ -1,7 +1,59 @@
 import React from "react";
+import Input from "../Forms/Input";
+import useForm from "../../Hooks/useForm";
+import Button from "../Forms/Button";
+import { PASSWORD_RESET } from "../../Api";
+import useFetch from "../../Hooks/useFetch";
+import Error from "../Helper/Error";
+import { useNavigate } from "react-router-dom";
 
 const LoginPasswordReset = () => {
-  return <div>LoginPasswordReset</div>;
+  const [login, setLogin] = React.useState("");
+  const [key, setKey] = React.useState("");
+  const password = useForm(); //Se eu quisesse validar um password forte era só passar 'password' como argumento para 'useForm()
+  const { error, loading, request } = useFetch();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const key = params.get("key");
+    const login = params.get("login");
+    if (key) setKey(key);
+    if (login) setLogin(login);
+  }, []);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (password.validate()) {
+      const { url, options } = PASSWORD_RESET({
+        login,
+        key,
+        password: password.value,
+      });
+      const { response } = await request(url, options);
+      if (response.ok) navigate("/login");
+    }
+  }
+
+  return (
+    <section>
+      <h1 className="title">Insira a Nova Senha</h1>
+      <form onSubmit={handleSubmit}>
+        <Input
+          label="Nova Senha"
+          type="password"
+          name="password"
+          {...password}
+        />
+        {loading ? (
+          <Button disabled>Trocando...</Button>
+        ) : (
+          <Button>Trocar Senha</Button>
+        )}
+      </form>
+      <Error error={error} />
+    </section>
+  );
 };
 
 export default LoginPasswordReset;
